8876c18de90f3d9883eaacc2c5bfa68c8ec82cc7
varaMb <varachkin.aliaksandr@mbicycle.com>
2022-08-16 12:49:57 +0300

    fix: fixed issues from comments

--- src/common/services/user-service/hooks/useUserPhoto.ts
+++ src/common/services/user-service/hooks/useUserPhoto.ts
@@ -3,9 +3,11 @@ import { useGetUserPhoto } from '../query-hooks';
 export const useUserPhoto = (): { photo: string; } => {
   const { data } = useGetUserPhoto();
 
-  const binaryData = [];
-  binaryData.push(data);
-  const photo = data ? URL.createObjectURL(new Blob(binaryData as Blob[])) : '';
+  const binaryData:Blob[] = [];
+  if (data) {
+    binaryData.push(data);
+  }
+  const photo = data ? URL.createObjectURL(new Blob(binaryData)) : '';
   return {
     photo,
   };

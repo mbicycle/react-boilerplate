bd05d2375cdb1473af8ed7e0d08180f5bdaced06
varaMb <varachkin.aliaksandr@mbicycle.com>
2022-08-19 15:45:41 +0300

    fix: does activeAcount

--- src/common/interceptors/axios.ts
+++ src/common/interceptors/axios.ts
@@ -26,7 +26,7 @@ axiosInstance.interceptors.request.use(async (request) => {
     request.headers.Authorization = `Bearer ${token}`;
     request.headers['Content-Type'] = 'application/json';
   }
-  // account.setActiveAccount();
+  account.setActiveAccount();
 
   return request;
 }, (error) => Promise.reject(error));
